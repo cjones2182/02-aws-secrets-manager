37493575b6c918619b8ff421f13b662f9b94e365
Cory Jones <cory@Corys-MacBook-Air.local>
2026-05-14 21:55:40 -0400

    added iam ec2 role for rds secret

--- modules:/secrets-manager:/iam.tf
+++ modules:/secrets-manager:/iam.tf
@@ -1,4 +1,6 @@
-
+# ===========================================================================================================
+# policy document for rds secret value (json in policy)
+# ===========================================================================================================
 
 data "aws_iam_policy_document" "rds_login" {
     statement {
@@ -9,9 +11,31 @@ data "aws_iam_policy_document" "rds_login" {
     actions   = ["secretsmanager:GetSecretValue"]
     resources = ["aws_secretsmanager_secret.rds_secret.arn"] #input db secret arn
   }
-
+# =========================================================================================================
+# actual policy with json attached to get rds secret value 
+# =========================================================================================================
 }
   resource "aws_secretsmanager_secret_policy" "name" {
     secret_arn = aws_secretsmanager_secret.rds_secret.arn
     policy = data.aws_iam_policy_document.rds_login.json
+  }
+  # ======================================================================================================
+  # role for RDS secret value  
+  # ===================================================================================================
+
+  resource "aws_iam_role" "ec2_role" {
+    name = "ec2-role"
+    assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
   }
